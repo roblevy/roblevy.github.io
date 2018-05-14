b3684309beb904500cc51242dda3ebbaf74edf99
Rob Levy <robert.g.levy@gmail.com>
2018-05-14 15:54:41 +0100

    The first GA mutability page

---
+++ pages/ga-ldn/BE18A9BF-C42C-40A4-9F8B-4DA7B3BCC29F.js
@@ -0,0 +1,7 @@
+let a = ['hello'],
+    b = a,
+    c = ['hello'];
+
+console.log('Behold! I tell you a mystery.');
+console.log('Have a look at the variables a, b and c');
+console.log('Which of them are equal (===)');
