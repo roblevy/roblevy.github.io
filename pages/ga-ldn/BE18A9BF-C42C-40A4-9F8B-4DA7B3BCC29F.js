e82d97be69763548fcb326fd61ab307d68da3029
Rob Levy <robert.g.levy@gmail.com>
2018-05-14 15:56:04 +0100

    Added a question mark

--- pages/ga-ldn/BE18A9BF-C42C-40A4-9F8B-4DA7B3BCC29F.js
+++ pages/ga-ldn/BE18A9BF-C42C-40A4-9F8B-4DA7B3BCC29F.js
@@ -4,4 +4,4 @@ let a = ['hello'],
 
 console.log('Behold! I tell you a mystery.');
 console.log('Have a look at the variables a, b and c');
-console.log('Which of them are equal (===)');
+console.log('Which of them are equal (===)?');
